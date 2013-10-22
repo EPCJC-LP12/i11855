df282b2ae1093911049aa1a85673687deee0f781
i11855 <i11855@epcjc.net>
2013-10-22 13:07:27 +0100

    Exercicio Restaurantes

---
+++ revisoes/Restaurantes/Restaurantes/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Database1DataSet = New Restaurantes.Database1DataSet()
+        Me.Database1DataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CriticasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CriticasTableAdapter = New Restaurantes.Database1DataSetTableAdapters.CriticasTableAdapter()
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Database1DataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CriticasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.DataSource = Me.CriticasBindingSource
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(52, 75)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Database1DataSet
+        '
+        Me.Database1DataSet.DataSetName = "Database1DataSet"
+        Me.Database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Database1DataSetBindingSource
+        '
+        Me.Database1DataSetBindingSource.DataSource = Me.Database1DataSet
+        Me.Database1DataSetBindingSource.Position = 0
+        '
+        'CriticasBindingSource
+        '
+        Me.CriticasBindingSource.DataMember = "Criticas"
+        Me.CriticasBindingSource.DataSource = Me.Database1DataSetBindingSource
+        '
+        'CriticasTableAdapter
+        '
+        Me.CriticasTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 272)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Database1DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Database1DataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CriticasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Database1DataSetBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Database1DataSet As Restaurantes.Database1DataSet
+    Friend WithEvents CriticasBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents CriticasTableAdapter As Restaurantes.Database1DataSetTableAdapters.CriticasTableAdapter
+
+End Class
